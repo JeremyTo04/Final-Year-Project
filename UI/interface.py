6a8956b4284f9110d483b9d91d874cdc62cc7138
elai0007 <elai0007@student.monash.edu>
2024-10-07 23:04:07 +0800

    update interface.py
    - removed unnecessary code

--- UI/interface.py
+++ UI/interface.py
@@ -1,8 +1,6 @@
 import gradio as gr
 import cv2
 import numpy as np
-# temporary 
-from fer import FER
 
 import importlib.util
 import sys
@@ -27,34 +25,8 @@ spec.loader.exec_module(module)
 # Now you can access run_training() from the module
 run_training = getattr(module, function_name)
 
-
-# testing with built in emotion detector
-emotion_detector = FER(mtcnn=True)
-
-# def detect_emotion(image):
-#     # convert image to BGR (OpenCV format)
-#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
-#     # emotion detection
-#     emotions = emotion_detector.detect_emotions(image)
-    
-#     if emotions:
-#         dominant_emotion = max(emotions[0]['emotions'], key=emotions[0]['emotions'].get)
-#         # display box around the face
-#         (x, y, w, h) = emotions[0]['box']
-#         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#         # text with emotion
-#         cv2.putText(image, dominant_emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    
-#     # converts back to RGB for display
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-#     return image, dominant_emotion if emotions else "No face detected"
-
 def detect_emotion(excel):
     return run_training(excel)
-
 
 # Gradio interface
 iface = gr.Interface(
